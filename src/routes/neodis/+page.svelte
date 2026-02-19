0b9d87df4a43078a9fe329a21151f54d2c784d75
felixq <felixqx@proton.me>
2026-02-19 22:07:46 +0100

    neodis ester egg

---
+++ src/routes/neodis/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import '../../app.css'
+import neodis from '/images/people-and-npcs/mods/comunity-relations/Neodis.avif'
+</script>
+
+<div class="flex items-center justify-center h-screen">
+    <div class="flex items-center justify-center h-screen">
+        <img src={neodis} alt="Neodis Avatar" width="max" height="max" class="inline-block fill-current ml-4" style="border-radius: 0.25rem;"/>
+        <div class="text-overlay">Neodis is bad he broke my lights</div>
+    </div>
+</div>
